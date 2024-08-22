e7b36e16ec9d65131fac146e7e170ff3e2dc2e75
imress_hack <raziel.sier@gmail.com>
2024-08-22 20:09:38 +0300

    banner and new filter

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // import './assets/styles/main.css'
 import './assets/styles/styles.css'
-
+import { initYandexMetrika } from 'yandex-metrika-vue3';
 import {createApp, watch} from "vue";
 import App from './App.vue'
 
 import { router } from "./Router/router.js";
 
 createApp(App)
-    .use(router)
+    .use(router, initYandexMetrika, {
+        id: 97936104,
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    })
     .mount('#app')
